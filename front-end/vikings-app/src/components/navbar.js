0bf3c6617cbcaaabd863254aac7863389739b6ce
Ricardo Mendes <ricardomendesprofissional@gmail.com>
2022-02-04 21:49:58 -0300

    Initial Project

---
+++ front-end/vikings-app/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div class="container">
+                <h1>Initial Project</h1>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
